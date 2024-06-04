a9cfd0c442c8880d744f6627243f0bd554229221
sty00a4-code <sty00a4-code@gmail.com>
2024-06-04 20:24:56 +0200

    moved to gist

--- packages/atf.lua
+++ packages/atf.lua
@@ -1,1 +1,1 @@
-return "https://pastebin.com/raw/kfvxKmHb"
+return "https://gist.githubusercontent.com/sty00a4-code/117b3f76827a3bb1205b8613e83755f2/raw/cb7f5bebbe8289110f474dd892fee823b11b864b/atf.lua"
